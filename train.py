dc9740c7b9da4629dcc4bf2d4fabc1221aeb3935
Tran Nhat Truong <tntruong196@gmail.com>
2026-06-10 19:38:06 +0700

    optimizer

--- train.py
+++ train.py
@@ -9,20 +9,22 @@ from torch.utils.data import DataLoader
 def main():
     DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
     print("now system use ", DEVICE)
-    BATCH_SIZE = 32
+    BATCH_SIZE = 16
     lr=0.0001
     epochs =  30
     raw_train = OxfordIIITPet(root="./data", split="trainval", target_types="segmentation", download=True)
     raw_val = OxfordIIITPet(root="./data", split="test", target_types="segmentation", download=True)
     train_dataset = PetDataset(raw_train, transform = train_transform)
     val_dataset = PetDataset(raw_val, transform = val_transform)
-    train_loader = DataLoader(train_dataset, BATCH_SIZE, shuffle =True, num_workers= 4, pin_memory=True)
-    val_loader = DataLoader(val_dataset,BATCH_SIZE,shuffle=False,num_workers=4, pin_memory= True)
+    train_loader = DataLoader(train_dataset, BATCH_SIZE, shuffle =True, num_workers= 2, pin_memory=True)
+    val_loader = DataLoader(val_dataset,BATCH_SIZE,shuffle=False,num_workers=2, pin_memory= True)
 
     model = UNet(num_classes=3,depth=5,merge_mode='concat').to(DEVICE)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr = lr)
     best_val_loss = float("inf")
+    scaler = torch.cuda.amp.GradScaler()
+
 
 
     for i in range(epochs):
@@ -34,10 +36,13 @@ def main():
         for inputs, labels in train_loader:
             inputs, labels = inputs.to(DEVICE), labels.to(DEVICE)
             optimizer.zero_grad()
-            outputs= model(inputs)
-            loss= criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+            with torch.cuda.amp.autocast():
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+            
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
             train_loss+=loss.item()*inputs.size(0)
             total_train+= inputs.size(0)
         
@@ -53,11 +58,13 @@ def main():
                 total_val += inputs.size(0)
         epoch_val_loss = val_loss/total_val
 
-        print(f"epoch {i+1}/{epochs} | train_loss: {epoch_train_loss:.4f} | val_loss: {epoch_val_loss:.4}")
+        print(f"epoch {i+1}/{epochs} | train_loss: {epoch_train_loss:.4f} | val_loss: {epoch_val_loss:.4f}")
         if epoch_val_loss < best_val_loss:
             best_val_loss = epoch_val_loss
             torch.save(model.state_dict(), "best_model.pth")
             print("already save best model in 'best_model.pth'")
+        torch.cuda.empty_cache()
+
 if __name__ =='__main__':
     main()
 
